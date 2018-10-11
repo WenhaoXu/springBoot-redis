f084acb5c784cf5a8e91dcfeb6e7a42c86bc505b
XUHO3 <howells.xu@oocl.com>
2018-10-11 14:01:43 +0800

    add rediTemplete for set object

---
+++ src/main/java/com/example/demo/RedisConfig.java
@@ -0,0 +1,136 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.jcache.config.JCacheConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends JCacheConfigurerSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+    @Autowired
+    private JedisConnectionFactory jedisConnectionFactory;
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator(){
+        return (target,method,params)->{
+            StringBuilder sb=new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(":");
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(":" + String.valueOf(obj));
+            }
+            String rsToUse = String.valueOf(sb);
+//            logger.info("自动生成Redis Key -> [{}]", rsToUse);
+            return rsToUse;
+        };
+    }
+
+    @Bean
+    @Override
+    public CacheManager cacheManager(){
+        RedisCacheManager.RedisCacheManagerBuilder builder=RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(jedisConnectionFactory);
+        return  builder.build();
+    }
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory){
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer=new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper=new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL,JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisTemplate<String,Object>redisTemplate=new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        RedisSerializer serializer=new StringRedisSerializer() ;
+        //key value 序列化
+        redisTemplate.setKeySerializer(serializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        //hashkey value 序列化
+        redisTemplate.setHashKeySerializer(serializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Override
+    @Bean
+    public CacheErrorHandler errorHandler(){
+        logger.info("init ->[{}]","Redis cacheError");
+        CacheErrorHandler cacheErrorHandler=new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+                logger.error("redis occur handleCacheGetError:key->[{}]",key,exception);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+                logger.error("redis occur handleCachePutError:key->[{}];value->[{}]",key,value,exception);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+                logger.error("redis occur handleCacheEvictError:key->[{}]",key);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+                logger.error("Redis occur handleCacheClearError：", exception);
+            }
+        };
+        return cacheErrorHandler;
+    }
+        @Bean
+        JedisConnectionFactory jedisConnectionFactory() {
+            logger.info("Create JedisConnectionFactory successful");
+            JedisConnectionFactory factory = new JedisConnectionFactory();
+            factory.setHostName("192.168.254.128");
+            factory.setPort(6500);
+            factory.setTimeout(10000);
+            factory.setPassword(password);
+            return factory;
+        }
+    @Bean
+    public JedisPool redisPoolFactory() {
+        logger.info("JedisPool init successful，host -> [{}]；port -> [{}]", "192.168.254.128", 6500);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(-1);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, "192.168.254.128", 6500, 100, password);
+        return jedisPool;
+    }
+
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,72 @@
 package com.example.demo;
 
+import com.alibaba.fastjson.JSON;
+import com.example.demo.domain.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+    private  static  final Logger logger=LoggerFactory.getLogger(DemoApplication.class);
+
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
     @Test
     public void contextLoads() {
+        User user=new User("wahaha",3);
+        redisTemplate.opsForValue().set(user.getName(),JSON.toJSONString(user));
+
     }
+
+    @Test
+    public void testMap(){
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("key1","value1");
+        map.put("key2","value2");
+        map.put("key3","value3");
+        map.put("key4","value4");
+        map.put("key5","value5");
+        redisTemplate.opsForHash().putAll("map1",map);
+        Map<String,String> resultMap = redisTemplate.opsForHash().entries("map1");
+        List<String> resultMapList = redisTemplate.opsForHash().values("map1");
+        Set<String> resultMapSet = redisTemplate.opsForHash().keys("map1");
+        String value = (String)redisTemplate.opsForHash().get("map1","key1");
+
+        logger.info("resultMap:"+resultMap);
+        logger.info("resultMapList:"+resultMapList);
+        logger.info("resultMapSet:"+resultMapSet);
+        logger.info("value:"+value);
+
+        /**
+         * 输出结果
+         * resultMap:{key2=value2, key5=value5, key4=value4, key1=value1, key3=value3}
+         * resultMapList:[value1, value2, value5, value3, value4]
+         * resultMapSet:[key1, key2, key5, key3, key4]
+         * value:value1
+         */
+
+    }
+
 
 }
